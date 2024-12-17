86f487ea5d540d77353c56f48d65e329953eb5ba
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-12-17 20:56:52 +0530

    upd : description

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,14 @@
+/*
+	Any Go File has this default structure -
+	1. A package name , which we are creating in this script (default is 'main'). A package is a way to group total go code
+	2. A single endpoint function in the name of package
+	3. Any imported packages (atleast 'fmt' - a standard library package)
+*/
+
 package main
 
 import "fmt"
 
-func main() { // Endpoint for default package (if no go.mod present)
+func main() {
 	fmt.Println("Hello World")
 }
