e3adec0d0ca7692e230cd8ef9bb79df5b4ea05b5
PARTHIB DEB <parthibkumardeb@gmail.com>
2025-03-24 12:06:16 +0530

    add : comments

--- src/Methods/method02.go
+++ src/Methods/method02.go
@@ -14,6 +14,11 @@ The following rules help you determine when to use each kind of receiver:
 1. If your method modifies the receiver, you must use a pointer receiver.
 2. If your method needs to handle nil instances , then it must use a pointer receiver.
 3. If your method doesn’t modify the receiver, you can use a value receiver.
+
+Pointer receivers work just like pointer function parameters; it’s a copy of the pointer that’s
+passed into the method. Just like nil parameters passed to functions, if you change the copy of
+the pointer, you haven’t changed the original. This means you can’t write a pointer receiver method
+that handles nil and makes the original pointer non-nil
 */
 
 type Counter struct {
