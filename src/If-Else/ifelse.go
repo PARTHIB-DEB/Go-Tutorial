9582450308b82e1261721b8738102f5c26b5bbdc
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2026-05-13 10:32:32 +0530

    Update ifelse.go

--- src/If-Else/ifelse.go
+++ src/If-Else/ifelse.go
@@ -26,33 +26,23 @@ func main() {
 		fmt.Println("That's a good number:", n)
 	}
 
+	
 	// Special Scoping in If-Else
 
-	if name := "Parthib"; name == "" { // Temporary used variable - name (its scope is limited to this if-else ladder)
-		fmt.Printf("No Name \n")
-	} else {
-		fmt.Printf("Name %s\n", name)
+func main(){
+    var k int;
+    fmt.Print("Enter number: ")
+    fmt.Scan(&k);
+	if k == 0{
+		fmt.Println("0 is not allowed as a divisor");
+		return;
 	}
-
-	if nk := rand.Intn(10); nk == 0 { // n is a temporary used variable , scope limited to if-else ladder
-		fmt.Println("That's too low")
-	} else if n > 5 {
-		fmt.Println("That's too big:", n)
-	} else {
-		fmt.Println("That's a good number:", n)
-	}
-
-	//fmt.Print(nk)   // undefined here so panick
-	//fmt.Println(name)  // undefined here so panick
-
-	// But watch this example below (It may be an error in this version of Go)
-
-	if n := rand.Intn(10); n == 0 {
-		fmt.Println("That's too low")
-	} else {
-		fmt.Println("That's a good number:", n)
-	}
-
-	fmt.Println(n) // This doesn't cause any error , No books cover this - It only happens with character 'n'
-
+    if n := rand.Intn(10); k == n % k && k <= n { // n is a temporary used variable , scope limited to if-else ladder
+        fmt.Println("That's too low")
+    } else if k > n  {
+        fmt.Println("That's too big:", n)
+    } else {
+        fmt.Println("That's a good number:", n)
+    }
+    // fmt.Print(n); // Undefined number 'n'
 }
